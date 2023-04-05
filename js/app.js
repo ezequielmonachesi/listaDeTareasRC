ecf54e6a841f00eacaf50fd9f8569dd911d336b5
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-04-05 17:40:26 -0300

    empezando logica borra tarea

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
-let form = document.querySelector('form');
-let textoIngresado = document.querySelector('#textInput')
+let form = document.querySelector("form");
+let textoIngresado = document.querySelector("#textInput");
 
-form.addEventListener('submit', agregarTarea)
+form.addEventListener("submit", agregarTarea);
+trash.addEventListener("click", borrarTarea);
 
-function agregarTarea(e){
-    e.preventDefault();
-    let tareasPendientes = document.querySelector('#tareasPendientes');
-    let datoIngresado = textoIngresado.value;
-    let liTareas = document.createElement('li');
-    liTareas.innerHTML = datoIngresado;
-    tareasPendientes.appendChild(liTareas);
+function borrarTarea() {
+    
+}
+
+function agregarTarea(e) {
+  e.preventDefault();
+  let datoIngresado = textoIngresado.value;
+  let liTareas = document.createElement("li");
+  liTareas.innerHTML = `
+    ${datoIngresado}
+    <i type="button" class="bi bi-trash-fill text-danger fs-5 ms-2"></i>
+    `;
+  liTareas.className = "d-flex flex-nowrap align-items-center";
+  tareasPendientes.appendChild(liTareas);
 }
