83ab825e6502cc384b39da97620555f8d104c3c4
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-04-11 18:14:48 -0300

    logica terminada

--- js/app.js
+++ js/app.js
@@ -1,21 +1,40 @@
 let form = document.querySelector("form");
 let textoIngresado = document.querySelector("#textInput");
+let todos = [];
+let borrar = document.querySelector("#borrarTarea");
 
 form.addEventListener("submit", agregarTarea);
-trash.addEventListener("click", borrarTarea);
+borrar.addEventListener("click", borrarTarea);
 
 function borrarTarea() {
-    
+  let deleteTodo = document.querySelector('#delete');
+  let valorInput = deleteTodo.value;
+  let buscarInput = todos.filter(i=>i === valorInput)
+  if(valorInput === buscarInput[0]){
+    let borrar = todos.indexOf(valorInput);
+    tareasPendientes.removeChild(tareasPendientes.children[borrar]);
+    todos.splice(borrar,1)
+  }else{
+    alert('El valor ingresado no está en las Tareas Pendientes')
+  }
 }
 
 function agregarTarea(e) {
   e.preventDefault();
   let datoIngresado = textoIngresado.value;
+  todos.push(datoIngresado);
+  recorrerArray();
+    textoIngresado.value='';
+}
+
+function recorrerArray(){
   let liTareas = document.createElement("li");
-  liTareas.innerHTML = `
-    ${datoIngresado}
-    <i type="button" class="bi bi-trash-fill text-danger fs-5 ms-2"></i>
+  todos.forEach((element, i) => {
+    liTareas.innerHTML = `
+    ${element}
+    <i type="button" class="bi bi-check text-success fs-2"></i>
     `;
-  liTareas.className = "d-flex flex-nowrap align-items-center";
-  tareasPendientes.appendChild(liTareas);
+    liTareas.className = `d-flex flex-row justify-content-center align-items-center ${i}`;
+    tareasPendientes.appendChild(liTareas);
+  });
 }
